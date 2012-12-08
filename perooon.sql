1ecbde8d7eb40738aea304ac02e5c2dad62a39a9
ysugimoto <neo.yoshiaki.sugimoto@gmail.com>
2012-12-08 23:21:17 +0900

    Added DB structure

---
+++ perooon.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- ホスト: localhost
+-- 生成日時: 2012 年 12 月 08 日 23:20
+-- サーバのバージョン: 5.5.20
+-- PHP のバージョン: 5.3.10
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- データベース: `perooon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `perooon`
+--
+
+CREATE TABLE IF NOT EXISTS `perooon` (
+  `times` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `perooon`
+--
+
+INSERT INTO `perooon` (`times`) VALUES
+(0);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
